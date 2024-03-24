e78421669e519997cbf4fb670dd7eed92a08c91b
Razvan Benga <razvanbenga89@gmail.com>
2024-03-24 11:59:38 +0200

    Built the Contacts view using mock data

--- PagoContacts/PagoContacts/PagoContactsApp.swift
+++ PagoContacts/PagoContacts/PagoContactsApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct PagoContactsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContactsView(model: ContactsModel())
     }
+  }
 }
